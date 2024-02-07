9554502e4118ac0b78c2e95ce2020a47e8736a03
MannyEngi <talk2cestoni@yahoo.com>
2024-02-06 16:06:34 -0800

    Typescript server, models, routes

---
+++ TodoList/Backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import todoRoutes from './routes/todoRoutes';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database Connection 
+mongoose.connect('your_mongodb_connection_string')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+// Routes
+app.use('/api/todos', todoRoutes);
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+});
