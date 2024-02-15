cf3fc0bcdb5d0c5043b138916a46db03dfe6d3b8
MannyEngi <talk2cestoni@yahoo.com>
2024-02-15 14:04:37 -0800

    Typescript todo

--- TodoList/Backend/src/controllers/todoController.ts
+++ TodoList/Backend/src/controllers/todoController.ts
@@ -14,3 +14,22 @@ export const getAllTodos = async(req: Request, res: Response): Promise<void> => 
         }
     }
 };
+
+export const createTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { title, completed } = req.body;
+        const newTodo = new Todo({
+            title,
+            completed: completed ?? false,
+        });
+        const savedTodo = await newTodo.save();
+        res.status(201).json(savedTodo);
+    } catch (err: unknown) {
+        if(err instanceof Error) {
+            res.status(400).json({message: err.message});
+        } else  {
+            res.status(400).json({ message: "An uknown error occupied" });
+        }
+    }
+};
+
